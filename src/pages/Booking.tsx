372102ee29d627b7eaa8da189bc59f5f3a0267ae
Screamm <davidrydgren@gmail.com>
2024-02-10 22:09:53 +0100

    Testing css Loader on booking page

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -1,3 +1,13 @@
+import { Loader } from "../components/Loader";
+
 export const Booking = () => {
-    return <>Booking</>;
+    return <>
+    
+    
+    Booking
+    
+    <Loader />
+    
+    
+    </>
   };
